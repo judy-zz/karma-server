dd58439761d464c7c7d7769118e4a624f658c0d8
Scott Woods <scott@westarete.com>
2009-08-31 16:01:52 -0400

    Update to latest webrat.

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -16,7 +16,7 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 config.gem 'cucumber',    :lib => false,        :version => '0.3.96'
-config.gem 'webrat',      :lib => false,        :version => '0.5.0' 
+config.gem 'webrat',      :lib => false,        :version => '0.5.1' 
 config.gem 'rspec',       :lib => false,        :version => '1.2.6' 
 config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.2.6' 
                                                                     
