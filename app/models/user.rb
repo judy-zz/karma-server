6a72b6cecd0d72c242e0c7344b05281f8d76c49a
Matt Simpson <matt.simpson3@gmail.com>
2009-09-08 15:27:08 -0500

    Annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  permalink  :string(255)     not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
   validates_presence_of   :permalink
   validates_uniqueness_of :permalink
