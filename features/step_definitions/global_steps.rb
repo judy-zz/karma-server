c39b1879f79119c63b3003510ae4d09e7ca6756d
Matt Simpson <matt.simpson3@gmail.com>
2009-09-29 13:33:57 -0500

    Organize the api steps out of global and into an api steps file

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -12,48 +12,6 @@ When /^I edit the ([^\"]*) with ([^\"]*) "([^\"]*)"$/ do |klass, method, value|
   visit "#{klass.pluralize.underscore}/#{value}/edit"
 end
 
-When /^I (GET|PUT|POST|DELETE|HEAD|OPTIONS|PROPFIND|TRACE) "([^\"]*)"( with body "(.*)")?$/ do |verb, path, clause, body|
-  # Perform the action at a precise time, so we can anticipate the resulting
-  # ActiveRecord timestamps.
-  at_time(Time.utc(2009,9,9, 12,0,0)) do
-    send verb.downcase.to_sym, path, body
-  end
-end
-
-Then /^I should get a (\d+) ([\w\s]+) response$/ do |code, name|
-  @response.code.should == code
-end
-
-Then /^the "(.*)" header should be "(.*)"$/ do |header_name, expected_value|
-  @response.headers[header_name].should == expected_value
-end
-
-Then /^I should get an? (HTML|JSON|XML) response$/i do |format|
-  content_type = case format.downcase
-  when 'html' then 'text/html'
-  when 'json' then 'application/json'
-  when 'xml'  then 'application/xml'
-  end
-  @response.content_type.should == content_type
-end
-
-Then /^I should get a JSON response body like:$/ do |string|
-  @response.content_type.should == 'application/json'
-  expected = ActiveSupport::JSON.decode(string)
-  actual   = ActiveSupport::JSON.decode(@response.body)
-  actual.should == expected
-end
-
-Then /^I should get an XML response body like:$/ do |string|
-  @response.content_type.should == 'application/xml'
-  expected = Hash.from_xml(string)
-  actual   = Hash.from_xml(@response.body)
-  actual.should == expected
-end
-Then /^I should get a blank response body$/ do
-  response.should be_empty
-end
-
 Given /^the following (\w+):$/ do |table_name, table|
   klass = table_name.classify.constantize
   table.hashes.each do |hash|
