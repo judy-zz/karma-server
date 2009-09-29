a9302870ade1a6466e55e769fe2690d0b1af24dc
Clinton Judy <clinton@westarete.com>
2009-09-29 12:06:09 -0400

    Added spec to buckets for dependent destroy. Cleaned user spec.

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -18,4 +18,23 @@ describe Bucket do
   it { should validate_presence_of(:permalink) }
   it { should validate_uniqueness_of(:permalink) }
 
+  describe "when there are some adjustments" do
+    before(:each) do
+      [-1, 3, 2, 3].each do |value|
+        a = Adjustment.new :value => value
+        a.user = User.make
+        a.bucket = @bucket
+        a.save!
+      end
+    end
+  
+    describe ":dependent => :destroy" do
+      it "should delete all adjustments when bucket is deleted" do
+        Adjustment.sum('value', :conditions => ["bucket_id = ?", @bucket.id]).should == 7
+        @bucket.destroy
+        Adjustment.sum('value', :conditions => ["bucket_id = ?", @bucket.id]).should == 0
+      end
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,6 +58,7 @@ describe User do
     
     describe ":dependent => :destroy" do
       it "should delete all adjustments when user is deleted" do
+        @user.karma.should == 7
         @user.destroy
         @user.karma.should == 0
       end
