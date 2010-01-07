9944cc51b18feb4f3d1ab3914a3fd87613b7d3a0
Scott Woods <scott@westarete.com>
2010-01-06 23:13:41 -0500

    Modernize capistrano recipe.

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -2,6 +2,9 @@
 # The directory that we're deploying to on the remote host.
 set :deploy_to, "/var/www/domains/karma.beta.westarete.com"
 
+# Non-standard ssh port.
+ssh_options[:port] = 22222
+
 # Tell capistrano to use the beta environment. This is key for running 
 # the database migrations via "cap staging deploy:migrations".
 set :rails_env, "beta"
