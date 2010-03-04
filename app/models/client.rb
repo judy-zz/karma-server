773efd037ed846464422b57957717958a6a2fff1
Ash Skutches and John Marino <developers+ash+john@westarete.com>
2010-03-04 17:35:50 -0500

    annotated so we can more easily undertand the models and specs

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,3 +3,17 @@
 class Client < ActiveRecord::Base
   belongs_to :website
 end
+
+# == Schema Information
+#
+# Table name: clients
+#
+#  id         :integer         not null, primary key
+#  hostname   :string(255)
+#  ip_address :string(255)
+#  api_key    :string(255)
+#  website_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -8,3 +8,17 @@ describe Client do
   it { should belong_to :website }
 
 end
+
+# == Schema Information
+#
+# Table name: clients
+#
+#  id         :integer         not null, primary key
+#  hostname   :string(255)
+#  ip_address :string(255)
+#  api_key    :string(255)
+#  website_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
