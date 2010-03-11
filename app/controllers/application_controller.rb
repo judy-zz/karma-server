7afe93a79078d891ec29fc3caf0b994c0e9cfdaa
John Marino <writejm@gmail.com>
2010-03-10 23:00:27 -0500

    added support for a current_admin and require_super_admin accessor for other classes/methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  helper_method :current_admin, :require_super_admin
   before_filter :authenticate
   
   # Turning off forgery protection for now since it was unexpectedly messing
@@ -11,10 +12,9 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  
-  protected
-  
-  
+
+protected
+    
   def rescue_action(exception)
     case exception
     when ActiveRecord::RecordNotFound, ActionController::RoutingError
@@ -33,8 +33,8 @@ class ApplicationController < ActionController::Base
     true
   end
   
-  private
-  
+private
+
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       if @current_admin = Admin.find_by_login(username)
@@ -44,6 +44,18 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+  
+  def current_admin
+    return @current_admin if defined?(@current_admin)
+    authenticate
+  end
+  
+  def require_super_admin
+    unless current_admin && current_admin.super_admin
+      flash[:failure] = "You must have sufficient privileges"
+      redirect_to :back
+      return false
+    end
+  end
 
 end
-
