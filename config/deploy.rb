62e2720c0d84ca97423b1dbd30557b6281673b20
Clinton Judy <clinton@westarete.com>
2010-09-23 16:24:38 -0400

    Fix problem with Gemfile including bundler.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,11 @@ require 'capistrano/ext/multistage'     # Support for multiple deploy targets
 require 'capistrano-helpers/branch'     # Ask user what tag to deploy
 require 'capistrano-helpers/passenger'  # Support for Apache passenger
 require 'capistrano-helpers/git'        # Support for git
+require 'capistrano-helpers/bundler'    # Install all required rubygems
 require 'capistrano-helpers/privates'   # Symlink private files after deploying
-require 'capistrano-helpers/gems'       # Install all required rubygems
 require 'capistrano-helpers/migrations' # Run all migrations automatically
 require 'capistrano-helpers/campfire'   # Post deploy info to campfire
+require 'hoptoad_notifier/capistrano'
 
 # The name of the application.
 set :application, "karma-server"
@@ -17,11 +18,10 @@ set :repository,  "git@github.com:westarete/karma-server.git"
 set :privates, %w{ 
   config/database.yml 
   config/initializers/session_store.rb
+  vendor/bundler
 }
-
 
 Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
   $: << File.join(vendored_notifier, 'lib')
 end
 
-require 'hoptoad_notifier/capistrano'
